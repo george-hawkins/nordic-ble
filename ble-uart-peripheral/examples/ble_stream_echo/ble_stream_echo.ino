f10bf9713ff1f17b27c3937dba74d4eb5c08d465
George Hawkins <george-hawkins@users.noreply.github.com>
2014-10-28 19:17:36 +0100

    Added .ino for Arduino IDE and removed unnecessary create-ino script.

---
+++ ble-uart-peripheral/examples/ble_stream_echo/ble_stream_echo.ino
@@ -0,0 +1,14 @@
+#include <SPI.h>
+#include <Streaming.h>
+#include <lib_aci.h>
+#include <BleCore.h>
+#include <BleUart.h>
+
+// These includes are not required by the C or C++ preprocessor.
+// They are required only as pointers for the Arduino IDE as to which libraries are required.
+
+// The real code for this example is in the corresponding .cpp file, which
+// should be open in the editor tab next to this one if using the Arduino IDE.
+
+// If using Eclipse or another IDE then this file can be ignored and the librares
+// should be included via whatever mechanism is appropriate for that IDE.
