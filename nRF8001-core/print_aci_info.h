197a1808587b9668be075e68be0472c767740b16
George Hawkins <george-hawkins@users.noreply.github.com>
2014-11-06 21:50:42 +0100

    Changed include guard name in print_aci_info.h.

--- nRF8001-core/print_aci_info.h
+++ nRF8001-core/print_aci_info.h
@@ -1,8 +1,8 @@
-#ifndef PRINT_ACI_EVT_H_
-#define PRINT_ACI_EVT_H_
+#ifndef PRINT_ACI_INFO_H_
+#define PRINT_ACI_INFO_H_
 
 #include <lib_aci.h>
 
 extern void printAciInfo(aci_state_t& aci_state, aci_evt_t& aci_evt);
 
-#endif /* PRINT_ACI_EVT_H_ */
+#endif /* PRINT_ACI_INFO_H_ */
